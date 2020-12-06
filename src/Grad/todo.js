21b61dff0dd67c5f205021b3eb6676649d8c014f
yjh96 <11yjh11@naver.com>
2020-12-06 22:58:40 +0900

    complete chrome_app

--- src/Grad/todo.js
+++ src/Grad/todo.js
@@ -1,6 +1,84 @@
+const TODOLS = "todo";
+
+const toDoList = document.getElementById("todoList");
+const toDoInput = document.getElementById("JS-todo");
+const toDoInputBox = document.getElementById("JS-todoInput");
+
 let toDo = [];
-let finished = [];
 
-function getTodo() {}
+function getTodo(value, id = null) {
+    const div = document.createElement("div");
+    div.classList.add("border-bottom");
+    div.classList.add("border-white-fade-15");
+    div.classList.add("p-2");
+    div.classList.add("mb-2");
+    div.classList.add("text-blue");
+    div.classList.add("text-emphasized");
+    div.classList.add("h2");
+    div.classList.add("text-left");
+    const delBtn = document.createElement("button");
+    delBtn.classList.add("btn-transparent");
+    delBtn.classList.add("btn-mktg");
+    delBtn.classList.add("border-0");
 
-function loadtoDo() {}
+    delBtn.innerText = "❌";
+    delBtn.addEventListener("click", removeToDo);
+    div.innerText = value;
+
+    div.appendChild(delBtn);
+    toDoList.appendChild(div);
+    if (id === null) {
+        id = Date.now();
+    } else {
+        div.id = id;
+    }
+    const item = {
+        id: id,
+        value: value,
+    };
+    toDo.push(item);
+    saveTodo(toDo);
+}
+
+function removeToDo(event) {
+    const btn = event.target;
+    const div = btn.parentNode;
+    const id = div.id;
+    const value = div.childNodes[0].data;
+
+    toDoList.removeChild(div);
+    const item = {
+        id: id,
+        value: value,
+    };
+
+    const cleanToDo = toDo.filter(function (item) {
+        return parseInt(item.id) !== parseInt(div.id);
+    });
+    toDo = cleanToDo;
+    saveTodo(toDo);
+}
+
+function saveTodo(value) {
+    localStorage.setItem(TODOLS, JSON.stringify(value));
+}
+
+function inputHandler(event) {
+    event.preventDefault();
+    const value = toDoInputBox.value;
+    getTodo(value);
+}
+
+toDoInput.addEventListener("submit", inputHandler);
+
+function loadToDo() {
+    const todo = localStorage.getItem(TODOLS);
+    if (todo !== null) {
+        const parser = JSON.parse(todo);
+        parser.forEach(function (todo) {
+            getTodo(todo.value, todo.id);
+        });
+    }
+}
+
+export { loadToDo };
--- src/Graduation_Project.js
+++ src/Graduation_Project.js
@@ -2,54 +2,14 @@ import { clock } from "./Grad/clock.js";
 import { background } from "./Grad/background.js";
 import { loadWeather } from "./Grad/weather.js";
 import { loadName } from "./Grad/name.js";
-
-//const nameForm = document.getElementById("nameInput"),
-// (nameElement = document.getElementById("JS-name")),
-//     (nameInputElement = document.getElementById("JS-nameInput"));
-
-// const NAMELS = "NAME";
-// const toDoLS = "toDo";
-
-// let toDoDB = [];
-// let nameDB = [];
-
-function inputHandler(event) {
-    event.preventDefault();
-    const value = event.target.childNodes[1].value;
-    console.log(event.target.childNodes[1].value);
-}
-
-// function background() {
-//     const img = new Image();
-//     const height = window.innerHeight;
-//     const width = window.innerWidth;
-//     img.src = `https://source.unsplash.com/random/${width}x${height}/?nature,winter,landscape `;
-//     img.classList.add("background");
-//     img.addEventListener("load", function () {
-//         body.appendChild(img);
-//     });
-// }
-
-function todoList() {}
-
-//nameForm.addEventListener("submit", inputHandler);
-
-function saveName(input) {}
-
-function name() {
-    const userName = localStorage.getItem(NAMELS);
-    if (userName !== "") {
-        nameElement.innerText = userName;
-    }
-}
-
-function weather() {}
+import { loadToDo } from "./Grad/todo.js";
 
 function init() {
     clock();
     background();
     loadWeather();
     loadName();
+    loadToDo();
     setInterval(clock, 1000);
 }
 
